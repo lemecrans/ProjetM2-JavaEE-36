0c17523f168ccd5a8c6a6c8ebcc5d046a2b380d3
Valisoa Ranaivoson <71868802+lemecrans@users.noreply.github.com>
2024-04-01 13:51:34 +0200

    Correctiion Utilisation d'une DataTable provenant de la librairie de composants JSF PrimeFaces

--- src/main/java/services/CustomerManager.java
+++ src/main/java/services/CustomerManager.java
@@ -2,17 +2,21 @@ package services;
 
 import app.gestioncli.entity.Customer;
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
 import jakarta.transaction.Transactional;
 import java.util.List;
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
 
 @RequestScoped
 public class CustomerManager {
     @PersistenceContext(unitName = "customerPU")
     private EntityManager em;
+
+    @Transactional
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
 
     public List<Customer> getAllCustomers() {
        Query query = em.createNamedQuery("Customer.findAll");
@@ -21,11 +25,6 @@ public class CustomerManager {
 
     @Transactional
     public Customer update(Customer customer) {
-       return em.merge(customer);
-    }
-
-    @Transactional
-    public void persist(Customer customer) {
-       em.persist(customer);
-    }
+        return em.merge(customer);
+    }  
 }
